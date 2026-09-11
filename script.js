76148120331fd3aa38765f36e4fde5ae7fe680f0
sejaumedaabc <sejaumeda.abcdaconstrucao@grupomysa.com.br>
2026-09-11 15:17:03 -0300

    upload js

--- script.js
+++ script.js
@@ -869,7 +869,19 @@ nextBtn.addEventListener(
     if (!validateForm()) {
       return;
     }
+    function formatarDataCadastro() {
+      const agora = new Date();
 
+      const dia = String(agora.getDate()).padStart(2, "0");
+      const mes = String(agora.getMonth() + 1).padStart(2, "0");
+      const ano = agora.getFullYear();
+
+      const hora = String(agora.getHours()).padStart(2, "0");
+      const minuto = String(agora.getMinutes()).padStart(2, "0");
+      const segundo = String(agora.getSeconds()).padStart(2, "0");
+
+      return `${dia}/${mes}/${ano} ${hora}:${minuto}:${segundo}`;
+    }
 
     // Monta os dados
     const data = {
@@ -898,8 +910,9 @@ nextBtn.addEventListener(
         $("cidadeFranqueado").value,
 
       ufFranqueado:
-        $("uf_cidadeFranqueado").value
+        $("uf_cidadeFranqueado").value,
 
+      dataCadastroLocal: formatarDataCadastro()
     };
 
 
